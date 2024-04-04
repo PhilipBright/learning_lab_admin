9a9e870fc8574a0a891314679bda6bccfc257074
Philip Bright <macbookpro2021@AppleClubs-MacBook-Pro.local>
2024-04-05 00:57:22 +0630

    feat: student table created

--- constant/data.ts
+++ constant/data.ts
@@ -1,3 +1,6 @@
+import { Payment } from "."
+
+
 export const navLink = [
     {
         name: 'Dashboard',
@@ -30,3 +33,33 @@ export const navLink = [
         icon: 'TeacherIcon'
     }
 ]
+export const data: Payment[] = [
+
+    {
+      id: "3u1reuv4",
+      username: "Philip Bright",
+      email: "Abe45@gmail.com",
+      enrollmentId: "3fdsdwe",
+      registerDate: "2019-01-30",
+      role: "Teacher",
+      subscription: "Free",
+    },
+    {
+      id: "derv1ws0",
+      username: "Emerald",
+      email: "Emerald@gmail.com",
+      enrollmentId: "56grdgf",
+      registerDate: "2020-12-01",
+      role: "Student",
+      subscription: "Premium",
+    },
+    {
+      id: "bhqecj4p",
+      username: "Bright Bright",
+      email: "Bright@gmail.com",
+      enrollmentId: "98fefdr",
+      registerDate: "2023-08-31",
+      role: "Teacher",
+      subscription: "Premium",
+    },
+  ]
